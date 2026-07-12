2dac70fe8a23be62c6d516b4b7b935b0f1d09be0
Mohataseem <mohataseem89@gmail.com>
2026-07-12 11:13:19 +0530

    message connection

---
+++ src/utils/socket.js
@@ -0,0 +1,8 @@
+import io from "socket.io-client";
+import { SOCKET_URL } from "./constants";
+
+export const createSocketConnection = () => {
+  return io(SOCKET_URL, {
+    withCredentials: true,
+  });
+};
